18cf8def3a57825a122738645093ddce68e88f59
iamNitin16 <nitinsharma95ns@gmail.com>
2017-12-19 23:40:14 +0530

    Added Todo component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('./components/TodoApp');
+
 // Load Foundation
 require('style-loader!css-loader!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
@@ -10,6 +12,6 @@ $(document).foundation();
 require('style-loader!css-loader!sass-loader!./styles/app.scss')
 
 ReactDOM.render(
-	<p>BoilerPlate 3 Project</p>,
+	<TodoApp />,
 	document.getElementById('app')
 );
